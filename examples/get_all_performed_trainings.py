7cd09092edb3875c8e550419550d5eec839f4919
mkb79 <mkb79@hackitall.de>
2023-04-19 14:21:49 +0200

    refactor: update example

--- examples/get_all_performed_trainings.py
+++ examples/get_all_performed_trainings.py
@@ -2,11 +2,12 @@ import asyncio
 import json
 import pathlib
 
-from freeletics import FreeleticsClient, AsyncFreeleticsClient
+from freeletics import AsyncFreeleticsClient, FreeleticsClient
 
-USERNAME = 'INSERT YOUR USERNAME'
-PASSWORD = 'INSERT YOUR PASSWORD'
-FILENAME = 'INSERT TARGET JSON FILENAME'
+
+USERNAME = "INSERT YOUR USERNAME"
+PASSWORD = "INSERT YOUR PASSWORD"  # noqa: S105
+FILENAME = "INSERT TARGET JSON FILENAME"
 
 
 async def async_main():
@@ -18,24 +19,21 @@ async def async_main():
         page = 1
         while True:
             r = await client.get_user_activities_by_id(page=page)
-            data = r['data']
+            data = r["data"]
             for i in data:
-                if i['type'] == 'training_completed':
-                    aod = i['relationships']['activity_object']['data']
-                    if aod['type'] == 'training':
-                        aids.append(aod['id'])
-            if 'next' not in r['links']:
+                if i["type"] == "training_completed":
+                    aod = i["relationships"]["activity_object"]["data"]
+                    if aod["type"] == "training":
+                        aids.append(aod["id"])
+            if "next" not in r["links"]:
                 break
             page += 1
 
         # get activities from ids
-        jobs = (
-            client.get_performed_activities_by_id(i) for i in aids
-        )
+        jobs = (client.get_performed_activities_by_id(i) for i in aids)
         activities = await asyncio.gather(*jobs)
         file = pathlib.Path(FILENAME)
-        activities = json.dumps(activities, indent=4,
-                                default=lambda o: o.as_dict())
+        activities = json.dumps(activities, indent=4, default=lambda o: o.as_dict())
         file.write_text(activities)
 
         await client.logout()
@@ -50,13 +48,13 @@ def sync_main():
         page = 1
         while True:
             r = client.get_user_activities_by_id(page=page)
-            data = r['data']
+            data = r["data"]
             for i in data:
-                if i['type'] == 'training_completed':
-                    aod = i['relationships']['activity_object']['data']
-                    if aod['type'] == 'training':
-                        aids.append(aod['id'])
-            if 'next' not in r['links']:
+                if i["type"] == "training_completed":
+                    aod = i["relationships"]["activity_object"]["data"]
+                    if aod["type"] == "training":
+                        aids.append(aod["id"])
+            if "next" not in r["links"]:
                 break
             page += 1
 
@@ -67,16 +65,14 @@ def sync_main():
             activities.append(r)
 
         file = pathlib.Path(FILENAME)
-        activities = json.dumps(activities, indent=4,
-                                default=lambda o: o.as_dict())
+        activities = json.dumps(activities, indent=4, default=lambda o: o.as_dict())
         file.write_text(activities)
 
         client.logout()
 
 
 # run async
-loop = asyncio.get_event_loop()
-loop.run_until_complete(async_main())
+asyncio.run(async_main())
 
 # run sync
 sync_main()
